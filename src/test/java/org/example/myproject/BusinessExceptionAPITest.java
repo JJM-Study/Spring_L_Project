e3f5b9c51c06b17e95ff4a40dd366a23aab10bda
JJM <jjmyoucandoit@gmail.com>
2025-11-27 10:05:44 +0900

    pj

--- src/test/java/org/example/myproject/BusinessExceptionAPITest.java
+++ src/test/java/org/example/myproject/BusinessExceptionAPITest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.myproject.auth.service.AuthService;
 import org.example.myproject.error.BusinessException;
 import org.example.myproject.error.ErrorCode;
+import org.example.myproject.order.dto.OrderRequestDto;
 import org.example.myproject.product.dto.ProductDto;
 import org.example.myproject.product.service.ProductService;
 import org.junit.jupiter.api.DisplayName;
@@ -21,8 +22,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -57,27 +58,32 @@ public class BusinessExceptionAPITest {
     @WithMockUser("MockTester")
     void orderExceptionJsonTest() throws Exception {
 
-        BusinessException mockException = new BusinessException(ErrorCode.STOCK_NOT_ENOUGH_DETAIL);
+        ProductDto productDto = new ProductDto();
+        productDto.setPrice(10000);
+        given(productService.selectNowOrdProduct(any())).willReturn(productDto);
 
-        ProductDto mockProduct = new ProductDto();
-        mockProduct.setPrice(10000);
+        given(authService.getAuthenticUserId(any())).willReturn("MockTester");
 
-
-        doThrow(mockException)
+        doThrow(new BusinessException(ErrorCode.STOCK_NOT_ENOUGH))
                 .when(orderService)
                 .createOrder(any(), any(), any(), any());
 
         Map<String, Object> simpleBody = new HashMap<>();
-        simpleBody.put("dummy", "data");
+        OrderRequestDto orderRequestDto = new OrderRequestDto();
+        orderRequestDto.setProdNo(1L);
+        orderRequestDto.setQty(2);
+        orderRequestDto.setTotalPrice(20000);
+
 
         mockMvc.perform(post("/order/order_prod")
                         .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(simpleBody)))
+                        .content(objectMapper.writeValueAsString(orderRequestDto)))
                 .andDo(print())
 
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.errorCode").value("STOCK_NOT_ENOUGH_DETAIL"));
+                .andExpect(jsonPath("$.code").value("S001"))
+                .andExpect(jsonPath("$.message").value("상품의 재고가 없습니다."));
 
         }
     }
